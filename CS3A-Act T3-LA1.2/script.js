d080e536bc9789761bdb8eda7beba86225893ed2
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2025-04-27 10:20:55 +0800

    Add files via upload

---
+++ CS3A-Act T3-LA1.2/script.js
@@ -0,0 +1,4 @@
+// JavaScript functionality can be added here if needed, e.g., for interactivity
+document.querySelector('.logout-btn').addEventListener('click', () => {
+    alert('Logging out...');
+  });
